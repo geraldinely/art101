dab34880bb8cfc5dd88be0998ed22d2b5a7f52c3
geraldinely <geraldinely2@gmail.com>
2024-11-10 18:48:29 -0800

    lab5
    created and completed lab 5: data types and variables.

---
+++ lab5/js/lab5.js
@@ -0,0 +1,19 @@
+// index.js - purpose and description here
+// Author: Geraldine Ly
+// Date: 11/10/2024
+
+// Define Variables
+make = "Toyota";
+model = "Prius Prime";
+color = "White";
+year = "2014";
+
+// calculate
+age = 2024 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
