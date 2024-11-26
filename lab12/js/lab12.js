c5545d3cc3d8865148ed975d7a5d02f3314082d7
geraldinely <geraldinely2@gmail.com>
2024-11-25 16:58:27 -0800

    lab 12
    created lab 12, created a program that assigns users to a group based on number of letters in their name using conditionals in javascript.

---
+++ lab12/js/lab12.js
@@ -0,0 +1,38 @@
+/*
+   lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Geraldine Ly
+   Date: 11/21/2024
+*/
+
+//Return Red Phoenix, Blue Dragons, Purple Turtles, Black Lions
+// depending on length mod 4
+
+function familySorting(str) {
+  let len = str.length;
+  let mod = len % 4;
+
+  if (mod === 0) {
+    return "red phoenix 🐦‍🔥";
+  } else if (mod === 1) {
+    return "blue dragons 🐉";
+  } else if (mod === 2) {
+    return "purple turtles 🐢";
+  } else if (mod === 3) {
+    return "black lions 🦁";
+  }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var family = familySorting(name);
+  
+  // Separate the text and family
+  let newText = "<p>You belong to...</p>";
+  let familyText = "<span class='family'>" + family + "</span>";
+
+  document.getElementById("output").innerHTML = newText + familyText;
+});
